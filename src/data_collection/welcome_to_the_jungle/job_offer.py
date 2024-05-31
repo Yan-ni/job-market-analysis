566bd13f03d5ee21f564c1641578e91aad5ec681
Anis K <anis01000001@gmail.com>
2024-05-31 14:59:59 +0200

    refactor job offer class

--- src/data_collection/welcome_to_the_jungle/job_offer.py
+++ src/data_collection/welcome_to_the_jungle/job_offer.py
@@ -1,4 +1,5 @@
 from .scraper import Scraper
+import re
 
 class JobOffer:
   """A representation of a welcome-to-the-jungle job offer.
@@ -8,32 +9,68 @@ class JobOffer:
   """
 
   def __init__(self, url: str):
-    self.url = url
+    self.url: str = url
+    self.__soup: BeautifulSoup = Scraper.get_url_soup(self.url)
 
-  def get_data(self) -> dict:
-    """Returns the data of the job offer."""
-    soup = Scraper.get_url_soup(self.url)
+    regex_search_result = re.search('https://www.welcometothejungle.com/en/companies/(?P<company_id>.*)/jobs/(?P<job_id>.*)\??.*', self.url)
+    self.id: str = regex_search_result.group('job_id')
+    self.company_id: str = regex_search_result.group('company_id')
+    self.title: str = self.__scrape_title()
+    self.description: str = self.__scrape_description()
+    self.preferred_experience: str = self.__scrape_preferred_experience()
+    self.recruitment_process: str = self.__scrape_recruitment_process()
 
-    job_data = dict()
+  def get_url(self) -> str:
+    return self.url
 
-    job_data['job_url'] = self.url
+  def get_id(self) -> str:
+    return self.id
 
-    job_title = soup.select_one('h1')
-    job_data['job_title'] = job_title.get_text(' ') if job_title else None
+  def get_title(self) -> str:
+    return self.title
 
-    company_name = soup.select_one('div + span')
-    job_data['company_name'] = company_name.get_text(' ') if company_name else None
+  def get_company_id(self) -> str:
+    return self.company_id
 
-    company_description = soup.select_one('section#about-section > h2 + div')
-    job_data['company_description'] = Scraper.get_soup_text(company_description) if company_description else None
+  def get_description(self) -> str:
+    return self.description
 
-    job_description = soup.select_one('section#description-section > h2 + div')
-    job_data['job_description'] = Scraper.get_soup_text(job_description) if job_description else None
+  def get_preferred_experience(self) -> str:
+    return self.preferred_experience
 
-    preferred_experience = soup.select_one('section#profile-section > h2 + div')
-    job_data['preferred_experience'] = Scraper.get_soup_text(preferred_experience) if preferred_experience else None
+  def get_recruitment_process(self) -> str:
+    return self.recruitment_process
 
-    recruitement_process = soup.select_one('section#recruitment-section > h2 + div')
-    job_data['recruitement_process'] = Scraper.get_soup_text(recruitement_process) if recruitement_process else None
+  def __scrape_title(self) -> str:
+    title_tag = self.__soup.select_one('h2')
 
-    return job_data
+    return title_tag.get_text(' ') if title_tag is not None else None
+
+  def __scrape_description(self) -> str:
+    description_tag = self.__soup.select_one('div[data-testid="job-section-description"] > div')
+
+    return Scraper.get_soup_text(description_tag) if description_tag is not None else None
+
+  def __scrape_preferred_experience(self) -> str:
+    preferred_experience_tag = self.__soup.select_one('div[data-testid="job-section-experience"] > div')
+
+    return Scraper.get_soup_text(preferred_experience_tag) if preferred_experience_tag is not None else None
+
+  def __scrape_recruitment_process(self) -> str:
+    recruitment_process_tag = self.__soup.select_one('div[data-testid="job-section-process"] > div')
+
+    return Scraper.get_soup_text(recruitment_process_tag) if recruitment_process_tag is not None else None
+
+  def to_dict(self):
+    return {
+      'id': self.get_id(),
+      'company_id': self.get_company_id(),
+      'title': self.get_title(),
+      'url': self.get_url(),
+      'description': self.get_description(),
+      'preferred_experience': self.get_preferred_experience(),
+      'recruitment_process':  self.get_recruitment_process()
+    }
+
+  def save_to_db(self):
+    pass
