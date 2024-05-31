22b0523b70aeacdab6500f83514ef0ae0d0fa0fa
Anis K <anis01000001@gmail.com>
2024-05-31 12:01:26 +0200

    refactor search page class

--- src/data_collection/main.py
+++ src/data_collection/main.py
@@ -5,22 +5,22 @@ from welcome_to_the_jungle import SearchPage, JobOffer, ScrapeDB
 scrape_db = ScrapeDB()
 
 # Variables
-total_pages_jobs_urls = set()
+total_job_offers_urls = set()
 
 # Scraping Welcome To The Jungle all search result pages
-search_page = SearchPage(page=1)
+search_page = SearchPage(page_number=1)
 while True:
-  page_jobs_urls = search_page.get_jobs_urls()
-  print("page {:<2} has {:<2} jobs".format(search_page.page, len(page_jobs_urls)))
-  total_pages_jobs_urls = total_pages_jobs_urls.union(page_jobs_urls)
+  page_job_offers_urls = search_page.get_jobs_offers_urls()
+  print("page {:<2} has {:<2} job offers".format(search_page.page_number, len(page_job_offers_urls)))
+  total_job_offers_urls = total_job_offers_urls.union(page_job_offers_urls)
 
-  if len(page_jobs_urls) < 30:
+  if len(page_job_offers_urls) < 30:
     break
-  search_page.next()
+  search_page.next_page()
 
 # Scraping all job offers
-for job_url in total_pages_jobs_urls:
-  job_offer_data = JobOffer(job_url).get_data()
+for job_offer_url in total_job_offers_urls:
+  job_offer_data = JobOffer(job_offer_url).get_data()
   scrape_db.save_job_offer_data(job_offer_data)
 
 # closing connection to the database
--- src/data_collection/welcome_to_the_jungle/search_page.py
+++ src/data_collection/welcome_to_the_jungle/search_page.py
@@ -12,21 +12,36 @@ class SearchPage:
   - page (int): The number of the page of the search result.
   """
 
-  def __init__(self, page=1, country_code="FR", contract_type="FULL_TIME", query="data analyst", location="Ile-de-France, France"):
+  def __init__(self, *, page_number=1, country_code="FR", contract_type="FULL_TIME", query="data analyst", location="Ile-de-France, France"):
     self.country_code = urllib.parse.quote(country_code)
     self.contract_type = urllib.parse.quote(contract_type)
     self.query = urllib.parse.quote(query)
     self.location = urllib.parse.quote(location)
-    self.page = page
+    self.page_number = page_number
+    self.url = f'https://www.welcometothejungle.com/en/jobs?refinementList%5Boffices.country_code%5D%5B%5D={self.country_code}&refinementList%5Boffices.state%5D%5B%5D=Ile-de-France&refinementList%5Bcontract_type%5D%5B%5D={self.contract_type}&query={self.query}&page={self.page_number}&aroundQuery={self.location}'
+
+  def get_country_code(self):
+    return self.country_code
+
+  def get_contract_type(self):
+    return self.get_contract_type
+
+  def get_query(self):
+    return self.query
+
+  def get_location(self):
+    return self.location 
+
+  def get_page_number(self):
+    return self.page_number
 
   def get_url(self) -> str:
-    """Returns the constructed url of the search page."""
-    return f"https://www.welcometothejungle.com/en/jobs?refinementList%5Boffices.country_code%5D%5B%5D={self.country_code}&refinementList%5Boffices.state%5D%5B%5D=Ile-de-France&refinementList%5Bcontract_type%5D%5B%5D={self.contract_type}&query={self.query}&page={self.page}&aroundQuery={self.location}"
+    return self.url
   
-  def get_jobs_urls(self) -> set[str]:
-    """Return the list of job urls present in the current search page."""
+  def get_jobs_offers_urls(self) -> set[str]:
+    """Return a set of job urls present in the current search page."""
     soup = Scraper.get_url_soup(self.get_url())
-    elements = soup.select('li > div > a')
+    elements = soup.select('li > div > div > a')
     jobs_urls = set()
 
     for element in elements:
@@ -35,6 +50,6 @@ class SearchPage:
 
     return jobs_urls
 
-  def next(self) -> None:
+  def next_page(self) -> None:
     """Sets the search page to the next search page."""
-    self.page += 1
+    self.page_number += 1
