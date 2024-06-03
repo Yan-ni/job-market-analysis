07616c1ad090507b353387271e32fb2f55993e99
Anis K <anis01000001@gmail.com>
2024-06-03 10:36:05 +0200

    update database queries

--- src/data_collection/welcome_to_the_jungle/database.py
+++ src/data_collection/welcome_to_the_jungle/database.py
@@ -3,7 +3,6 @@ import time
 
 class ScrapeDB:
   """A representation of the database that holds the scraping data."""
-
   con = psycopg2.connect(user="postgres", password="password", host="localhost")
   cur = con.cursor()
   cur.execute("""CREATE TABLE IF NOT EXISTS scrapes(
@@ -12,13 +11,12 @@ class ScrapeDB:
       ended_at INTEGER)"""
     )
 
-  self.cur.execute("""CREATE TABLE IF NOT EXISTS job_offers(
+  cur.execute("""CREATE TABLE IF NOT EXISTS job_offers(
       id SERIAL PRIMARY KEY,
-      job_url TEXT,
-      job_title TEXT,
-      company_name TEXT,
-      company_description TEXT,
-      job_description TEXT,
+      company_id TEXT,
+      title TEXT,
+      url TEXT,
+      description TEXT,
       preferred_experience TEXT,
       recruitment_process TEXT,
       scrape_id INTEGER,
@@ -29,12 +27,13 @@ class ScrapeDB:
   con.commit()
 
   scrape_id = cur.lastrowid
+  print('[INFO] database ready!')
 
   @classmethod
   def close(cls) -> None:
     """Updates the scrape end time in the data and closes the cursor and the database."""
     timestamp = int(time.time())
-    self.cur.execute('UPDATE scrapes SET ended_at=%s WHERE id=%s', [timestamp, cls.scrape_id])
-    self.con.commit()
-    self.cur.close()
-    self.con.close()
+    cls.cur.execute('UPDATE scrapes SET ended_at=%s WHERE id=%s', [timestamp, cls.scrape_id])
+    cls.con.commit()
+    cls.cur.close()
+    cls.con.close()
