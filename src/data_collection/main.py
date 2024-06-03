fd136e323c887e886511034df3247cb33080ffa3
Anis K <anis01000001@gmail.com>
2024-06-03 10:07:40 +0200

    update main script

--- src/data_collection/main.py
+++ src/data_collection/main.py
@@ -1,27 +1,32 @@
 # Imports
 from welcome_to_the_jungle import SearchPage, JobOffer, ScrapeDB
 
-# Init
-scrape_db = ScrapeDB()
-
 # Variables
 total_job_offers_urls = set()
 
 # Scraping Welcome To The Jungle all search result pages
 search_page = SearchPage(page_number=1)
+
+print('[PROCESS] retrieving job offers list from search page.')
+print('{:<8} {:<8}'.format('Page', 'Job offers'))
+
 while True:
   page_job_offers_urls = search_page.get_jobs_offers_urls()
-  print("page {:<2} has {:<2} job offers".format(search_page.page_number, len(page_job_offers_urls)))
+
+  print("{:<8} {:<8}".format(search_page.get_page_number(), len(page_job_offers_urls)))
+
   total_job_offers_urls = total_job_offers_urls.union(page_job_offers_urls)
 
   if len(page_job_offers_urls) < 30:
     break
+
   search_page.next_page()
+
+print('[PROCESS] retrieving job offers data.')
 
 # Scraping all job offers
 for job_offer_url in total_job_offers_urls:
-  job_offer_data = JobOffer(job_offer_url).get_data()
-  scrape_db.save_job_offer_data(job_offer_data)
+  JobOffer(job_offer_url).save_to_db()
 
 # closing connection to the database
-scrape_db.close()
+ScrapeDB.close()
