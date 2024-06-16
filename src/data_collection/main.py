b2d5cba6b6aa4973fa34d7f0158fe4ed9238b97f
Anis K <anis01000001@gmail.com>
2024-06-16 14:16:25 +0200

    refactor logging

--- src/data_collection/main.py
+++ src/data_collection/main.py
@@ -3,12 +3,15 @@ from welcome_to_the_jungle import SearchPage, JobOffer, ScrapeDB
 import concurrent.futures
 from dotenv import load_dotenv
 import os
+import logging
+import argparse
+import traceback
 
 # Functions
 def process_job_offer(job_offer_url):
   try:
     db_pool = ScrapeDB.get_pool()
-    print('[DATABASE] GETTING DB CONNECTION FROM DB POOL')
+    logging.debug('getting DB connection from DB POOL.')
     db_connection = db_pool.getconn()
     db_cursor = db_connection.cursor()
 
@@ -19,18 +22,19 @@ def process_job_offer(job_offer_url):
       if not job_offer_company.exists_in_db():
         job_offer_company.scrape_all_attributes()
         job_offer_company.save_to_db()
-        print(f'[DATABASE] saving the "company":"{job_offer_company.get_name()}" data in the database.')
+        logging.debug(f'saving the "company":"{job_offer_company.get_name()}" data in the database.')
 
       job_offer.scrape_all_attributes()
-      print(f'[DATABASE] saving the "job offer":"{job_offer.get_title()}" data in the database.')
+      logging.debug(f'saving the "job offer":"{job_offer.get_title()}" data in the database.')
       job_offer.save_to_db()
     else:
-      print(f'[INFO] "job offer":"({job_offer.get_id()}, {job_offer_company.get_id()})" already exists!')
-    print('[DATABASE] PUTTING AWAY DATABASE CONNECTION CONNECTION')
+      logging.debug(f'"job offer":"({job_offer.get_id()}, {job_offer_company.get_id()})" already exists!')
+    logging.debug('putting away the DB connection.')
     db_cursor.close()
     db_pool.putconn(db_connection)
   except Exception as e:
-    print(f'[ERROR] An error occurred: {e}')
+    logging.error(f'An error occurred while processing job offer: {e}')
+    logging.error(traceback.format_exc())
 
 def check_environment_variables():
   load_dotenv()
@@ -52,7 +56,25 @@ def check_environment_variables():
   if POSTGRES_HOSTNAME is None:
     raise Exception(f"postgres database name can't be {POSTGRES_HOSTNAME}")
 
+def parse_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-d', '--debug', help="sets logging level to debug", action="store_true")
+  
+  args = parser.parse_args()
+  
+  return args
+
 def main():
+  args = parse_arguments()
+
+  if args.debug is True:
+    logging_level = logging.DEBUG
+
+  logging.basicConfig(
+    level=logging_level,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+  )
   check_environment_variables()
 
   total_job_offers_urls = set()
@@ -65,13 +87,13 @@ def main():
   # Scraping Welcome To The Jungle all search result pages
   search_page = SearchPage(db_cursor=db_cursor)
 
-  print('[PROCESS] retrieving job offers list from search page.')
-  print('{:<8} {:<8}'.format('Page', 'Job offers'))
+  logging.info('retrieving job offers list from search page...')
+  logging.debug('{:<8} {:<8}'.format('Page', 'Job offers'))
 
   while True:
     page_job_offers_urls = search_page.get_jobs_offers_urls()
 
-    print("{:<8} {:<8}".format(search_page.get_page_number(), len(page_job_offers_urls)))
+    logging.debug("{:<8} {:<8}".format(search_page.get_page_number(), len(page_job_offers_urls)))
 
     total_job_offers_urls = total_job_offers_urls.union(page_job_offers_urls)
 
@@ -80,10 +102,12 @@ def main():
 
     search_page = search_page.next_page()
 
+  logging.info(f'retrieved {len(total_job_offers_urls)} job offers from {search_page.get_page_number()} pages.')
+
   db_cursor.close()
   db_connection.close()
 
-  print('[PROCESS] retrieving job offers data.')
+  logging.info('retrieving job offers data...')
 
   with concurrent.futures.ProcessPoolExecutor() as executor:
     executor.map(process_job_offer, total_job_offers_urls)
