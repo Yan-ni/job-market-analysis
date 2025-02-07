3e69e51159eae58de5f75f724bfeb9a7be714e71
Anis K <anis.kheddar@witco.io>
2025-02-07 15:19:20 +0100

    feat (cleaner): Add data cleaner to load in silver stage

---
+++ scripts/clean_data.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Global variables
+DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+DOCKER_COMPOSE_FILE="$DIR_PATH/../docker-compose.yml"
+
+docker compose -f $DOCKER_COMPOSE_FILE up -d clean-data
--- scripts/load_cron.sh
+++ scripts/load_cron.sh
@@ -5,10 +5,13 @@ DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DA_MAIN_SCRIPT_FILE="$DIR_PATH/data_collection.sh --container da-script"
 DS_MAIN_SCRIPT_FILE="$DIR_PATH/data_collection.sh --container ds-script"
 UPDATE_DELETED_SCRIPT_FILE="$DIR_PATH/update_deleted.sh"
+CLEAN_DATA_SCRIPT_FILE="$DIR_PATH/clean_data.sh"
 TEMP_CRON="/tmp/crontab_temp"
 
 # Script
 crontab -l > "$TEMP_CRON"
+
+echo "0 0 * * * $CLEAN_DATA_SCRIPT_FILE" >> "$TEMP_CRON" # Every day at midnight
 
 echo "0 1 * * * $DA_MAIN_SCRIPT_FILE" >> "$TEMP_CRON" # Every day at 01:00
 
