76467bf33ede7f1f46d76648b80e46a15ca4fe0c
Anis K <45605513+Yan-ni@users.noreply.github.com>
2025-02-08 23:27:18 +0100

    feat(scrape): Add data engineer position to scraping (#14)

--- scripts/load_cron.sh
+++ scripts/load_cron.sh
@@ -4,6 +4,7 @@
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DA_MAIN_SCRIPT_FILE="$DIR_PATH/data_collection.sh --container da-script"
 DS_MAIN_SCRIPT_FILE="$DIR_PATH/data_collection.sh --container ds-script"
+DE_MAIN_SCRIPT_FILE="$DIR_PATH/data_collection.sh --container de-script"
 UPDATE_DELETED_SCRIPT_FILE="$DIR_PATH/update_deleted.sh"
 CLEAN_DATA_SCRIPT_FILE="$DIR_PATH/clean_data.sh"
 TEMP_CRON="/tmp/crontab_temp"
@@ -17,7 +18,9 @@ echo "0 1 * * * $DA_MAIN_SCRIPT_FILE" >> "$TEMP_CRON" # Every day at 01:00
 
 echo "0 3 * * * $DS_MAIN_SCRIPT_FILE" >> "$TEMP_CRON" # Every day at 03:00
 
-echo "0 5 * * * $UPDATE_DELETED_SCRIPT_FILE" >> "$TEMP_CRON" # Every day at 05:00
+echo "0 5 * * * $DE_MAIN_SCRIPT_FILE" >> "$TEMP_CRON" # Every day at 05:00
+
+echo "0 7 * * * $UPDATE_DELETED_SCRIPT_FILE" >> "$TEMP_CRON" # Every day at 07:00
 
 
 crontab "$TEMP_CRON"
