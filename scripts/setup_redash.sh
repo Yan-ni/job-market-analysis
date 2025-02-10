7f9450b60889512cc2f142e45e8c2f75123e7e8b
Anis K <45605513+Yan-ni@users.noreply.github.com>
2025-02-10 13:23:16 +0100

    feat (docker): refactor docker files (#21)

--- scripts/setup_redash.sh
+++ scripts/setup_redash.sh
@@ -3,6 +3,6 @@
 # Global variables
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-DOCKER_COMPOSE_FILE="$DIR_PATH/../redash.docker-compose.yml"
+DOCKER_COMPOSE_FILE="$DIR_PATH/../docker-compose.redash.yml"
 
 docker compose -f $DOCKER_COMPOSE_FILE run --rm redash-server create_db
