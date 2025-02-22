56c3e0e95159053be9d50802d02e0e21381ae0bf
Anis K <45605513+Yan-ni@users.noreply.github.com>
2025-02-22 13:59:47 +0100

    fix (cron): Fix data cleaner cron script (#26)

--- scripts/clean_data.sh
+++ scripts/clean_data.sh
@@ -5,4 +5,4 @@ DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DOCKER_COMPOSE_FILE="$DIR_PATH/../docker-compose.yml"
 
-docker compose -f $DOCKER_COMPOSE_FILE up -d clean-data
+docker compose -f $DOCKER_COMPOSE_FILE up -d data-cleaner
