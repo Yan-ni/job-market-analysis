feca88244df6ceae1bf53ca46eb155ca8e6e453b
Anis K <45605513+Yan-ni@users.noreply.github.com>
2025-05-14 20:16:15 +0200

    fix (settings): remove database from settings (#71)

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    postgres_database: str
     postgres_password: str
     postgres_user: str
     postgres_hostname: str
