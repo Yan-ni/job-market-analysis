cc2e18ccd9c9e9a6d1ae84b06a8d0410e2a37ed0
Anis K <45605513+Yan-ni@users.noreply.github.com>
2025-05-14 15:35:10 +0200

    fix (import): remove unexistant function (#69)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 # Imports
 from helper.functions import scrape_jobs, update_deleted
-from utils.functions import check_env_var, config_logging, parse_arguments
+from utils.functions import config_logging, parse_arguments
 
 
 def main():
     args = parse_arguments()
     config_logging(args.debug)
-    check_env_var()
 
     if args.update_deleted:
         update_deleted()
